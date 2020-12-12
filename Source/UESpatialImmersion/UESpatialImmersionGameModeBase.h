71d713e0ec4f40990be2265fd1deb2c31fc997f6
Saeed Safikhani <s.safikhani@outlook.com>
2020-12-12 19:01:49 +0100

    Initial Commit

---
+++ Source/UESpatialImmersion/UESpatialImmersionGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UESpatialImmersionGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UESPATIALIMMERSION_API AUESpatialImmersionGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
